663c687f47b4cdacb18d8b0cdfb7e77d9788b663
William <williamfraga@gec.inatel.br>
2020-11-22 23:15:02 -0300

    auth test

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 import * as restify from 'restify';
+const axios = require('axios');
 
 const SERVER_PORT = 8080;
- 
+
 const server = restify.createServer();
- 
+
 server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
@@ -13,23 +14,30 @@ server.get('/', (req, res, next) => {
 
     return next();
 });
- 
+
 server.get('/alo/:name', (req, res, next) => {
     res.send('alo ' + req.params.name);
     return next();
 });
 
-server.post('/alo', (req, res, next) => {
-    const body = req.body;
-    if (body && body.name) {
-        res.send(`fala tu ${body.name}`);
-    } else {
-        res.send(500);
-    }
+server.post('/login', (req, res, next) => {
+    const { username } = req.body;
+    const { password } = req.body;
+
+    axios.post('https://ec021-av2-auth.herokuapp.com/auth/login', {
+        username,
+        password
+        })
+        .then(function (response) {
+            return res.json(response.status, response.data);
+        })
+        .catch(function (error) {
+            return res.json(error.response.status, error.response.data);
+        });
 
     return next();
 });
 
 server.listen(SERVER_PORT, () => {
-  console.log('%s listening at %s', server.name, server.url);
+    console.log('%s listening at %s', server.name, server.url);
 });
